e448da5472fa2908df4d4b541b2e70069d9945f3
Thomas Walsh <126Twalsh@frhsd.com>
2025-10-16 08:34:57 -0400

    Day2

--- js/Script.js
+++ js/Script.js
@@ -1,2 +1,9 @@
-document.getElementById("NBA").onclick= function() {myFunction()};
+document.getElementById("NBA").onclick = function() {NBAFunction()};
 
+var a = document.getElementById("DisplayV");
+var b = document.getElementById("NBAv");
+function NBAFunction() {
+    a.src = "img/Fortnight.mp4"
+    a.load();
+    console.log ("CLICK");
+}
